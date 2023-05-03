e50e08b4d82385852ef19a812ff9baab0647ff70
Mifrah Malik <mifrah.malik@gmail.com>
2023-05-03 16:04:08 -0400

    set up stories as part of my default route. Might have to add another route for auth later on. For now will focus on connecting my external API to see data print in an ideal manner

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import ListJobs from './components/ListJobs.jsx'
 import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router} />
+   <App />
   </React.StrictMode>,
 )
