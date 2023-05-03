8f76cdcce18fd1e1875db9803149e4d2578bc9e8
Mifrah Malik <mifrah.malik@gmail.com>
2023-05-03 17:10:33 -0400

    connected data from API with react_router but need to go back and hide the app id and app key

--- client/src/components/ListJobs.jsx
+++ client/src/components/ListJobs.jsx
@@ -15,7 +15,8 @@ const ListJobs = () => {
     fetch("http://localhost:8080/api/swejobs")
       .then((response) => response.json())
       .then((jobs) => {
-        setJobs(jobs);
+        // console.log(jobs);
+        setJobs(jobs.results);
       });
   };
 
