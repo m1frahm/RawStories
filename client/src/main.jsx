ede7dd0b11ba3e64990a35c30cb1bf816ef6f728
Mifrah Malik <mifrah.malik@gmail.com>
2023-05-05 13:20:59 -0400

    Added .env file to client side

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Auth0Provider} from `@auth0/auth0-react`;
 import ListJobs from './components/ListJobs.jsx'
 import './index.css'
 
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <App />
+   <Auth0Provider
+    // domain={DOMAIN}
+    // clientId={CLIENTID}
+    // authorizationParams={{
+    //   redirect_uri: window.location.origin,
+    // }}
+    >
+    <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
