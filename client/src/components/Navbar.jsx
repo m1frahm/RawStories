7377424a2d5c5a8e8d751d1797f75ee28133776e
Mifrah Malik <mifrah.malik@gmail.com>
2023-05-08 15:52:34 -0400

    adding login button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,36 +1,35 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Logo from '../assets/BlueTechtonicaWord.png'
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Logo from "../assets/BlueTechtonicaWord.png";
 import { useAuth0 } from "@auth0/auth0-react";
 
-
-
 function MyNavBar(props) {
+  const { loginWithRedirect } = useAuth0();
 
   return (
     <>
-    <Navbar data-testid="navbar" bg="dark" variant="dark" sticky="top">
-      <Container>
-        <Navbar.Brand href="/">
-        <img
+      <Navbar data-testid="navbar" bg="dark" variant="dark" sticky="top">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
               src={Logo}
               height="30"
               className="d-lg-inline-block"
               alt="React Bootstrap logo"
             />
-        </Navbar.Brand>
-        <Nav.Link >Your Link</Nav.Link>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Cristina Rodriguez</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Brand>
+          <Nav.Link>Your Link</Nav.Link>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+            <button onClick={() => loginWithRedirect()}>Log In</button>;
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
-};
+}
 
 export default MyNavBar;
