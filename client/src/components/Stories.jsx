3c5d13d3500f644b778031f63edabccfe325a22d
Mifrah Malik <mifrah.malik@gmail.com>
2023-05-09 17:14:43 -0400

    removed two lines of code instead of commenting them out

--- client/src/components/Stories.jsx
+++ client/src/components/Stories.jsx
@@ -5,8 +5,6 @@ import Logo from "../assets/BlueTechtonicaWord.png";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function Stories() {
-  // const { user, isAuthenticated, isLoading } = useAuth0(); //testing purposes only
-  // console.log(user); //shows what user information auth gives
   return <div>This is the homepage that will host stories main page</div>;
 }
 
