7c47fc2b8bf8d3d3015b2ba1bfe9d5ecc88c1abe
Mifrah Malik <mifrah.malik@gmail.com>
2023-05-16 16:22:50 -0400

    Styled individual stories

--- client/src/pages/IndividualStoryPage.jsx
+++ client/src/pages/IndividualStoryPage.jsx
@@ -3,8 +3,9 @@
 // /stories/<storyId> -> story
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useStory } from '../hooks/useStory'
+import { useStory } from "../hooks/useStory";
 import Title from "../components/Header";
+import { Segment } from "semantic-ui-react";
 
 export default function IndividualStoryPage() {
   const { storyId = "" } = useParams();
@@ -14,21 +15,27 @@ export default function IndividualStoryPage() {
     return <div>Loading...</div>;
   }
 
-//Individual Story Page
+  //Individual Story Page
   return (
     <div>
-    <Title/>
-      
-      <b><h1>{story.post_title}</h1> </b>
-      <h5><br></br>
-      Name: {story.interview_person_name}
-      <br></br>
-      Occupation: {story.interview_person_occupation}
-      <br></br>
-      Alma Matter: {story.interview_person_alma}</h5>
-      <br></br>
-      {story.post_body}
-      
+      <Title />
+      <div className="StoryDisplay">
+        <Segment padded="very" textAlign="left">
+          <b>
+            <h1>{story.post_title}</h1>{" "}
+          </b>
+          <h5>
+            <br></br>
+            Name: {story.interview_person_name}
+            <br></br>
+            Occupation: {story.interview_person_occupation}
+            <br></br>
+            Alma Matter: {story.interview_person_alma}
+          </h5>
+          <br></br>
+          {story.post_body}
+        </Segment>
+      </div>
     </div>
   );
 }
